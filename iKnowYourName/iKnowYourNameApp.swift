6bfde1248ce96a2085993ff2be0c878a67034112
natashasosa <natasha.sosa@poatek.com>
2023-09-22 15:10:39 -0300

    First commit

---
+++ iKnowYourName/iKnowYourNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKnowYourNameApp.swift
+//  iKnowYourName
+//
+//  Created by Natasha Rebelo on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iKnowYourNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
